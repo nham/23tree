5754f2b61271dcbcdb6307da53a900b09ba3cb89
nham <hamann.nick@gmail.com>
2014-07-14 14:54:09 -0400

    New Tree23 design

--- 23.rs
+++ 23.rs
@@ -3,8 +3,8 @@ type Link<T> = Option<Box<T>>;
 struct Node2<K, V> {
     key: K,
     val: V,
-    link1: Link<TwoThreeNode<T>>,
-    link2: Link<TwoThreeNode<T>>,
+    left: Link<Node23<K, V>>,
+    right: Link<Node23<K, V>>,
 }
 
 struct Node3<K, V> {
@@ -12,24 +12,43 @@ struct Node3<K, V> {
     val1: V,
     key2: K,
     val2: V,
-    link1: Link<TwoThreeNode<T>>,
-    link2: Link<TwoThreeNode<T>>,
-    link3: Link<TwoThreeNode<T>>,
+    left: Link<Node23<K, V>>,
+    middle: Link<Node23<K, V>>,
+    right: Link<Node23<K, V>>,
 }
 
-enum Node23<T> {
-    Node2(TwoNode<T>),
-    Node3(ThreeNode<T>),
+enum Node23<K, V> {
+    Node2(Node2<K, V>),
+    Node3(Node3<K, V>),
 }
 
-enum Tree23<T> {
-    Nil,
-    Node(Link<Node23<T>>),
-}
-
-impl<K: Ord, V> Tree23<K, V> {
-    fn get(&self, key: K) -> Option<V> {
-
+impl<K, V> Node23<K, V> {
+    fn leaf2(key: K, val: V) -> Node23<K, V> {
+        Node2(Node2 { key: key, val: val, 
+                      left: None, right: None, size: 1 })
     }
 
+    fn leaf3(key1: K, val1: V, key2: K, val2: V) -> Node23<K, V> {
+        Node3(Node3 { key1: key1, val1: val1, key2: key2, val2: val2, 
+                      left: None, middle: None, right: None, size: 1 })
+    }
+
+    fn is2(&self) -> bool {
+        match *self {
+            Node2(_) => true,
+            _        => false,
+        }
+    }
+
+    fn is3(&self) -> bool {
+        match *self {
+            Node3(_) => true,
+            _        => false,
+        }
+    }
+}
+
+
+struct Tree23<K, V> {
+    root: Option<Node23<K, V>>,
 }
