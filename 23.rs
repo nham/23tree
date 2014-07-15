f218f82268c03b43c38b25deb5ae0d98756067ba
nham <hamann.nick@gmail.com>
2014-07-15 19:47:47 -0400

    Change the implementation 2-3 trees in the sense of Aho et al.
    I was following along Sedgewick, whose 2-3 trees are actually 2-3
    B-Trees, not what I want. This defines a new structure for 2-3 trees and
    implements the get method.

--- 23.rs
+++ 23.rs
@@ -1,92 +1,72 @@
-type Link<T> = Option<Box<T>>;
-
-struct Node2<K, V> {
+struct LeafNode<K, V> {
     key: K,
     value: V,
-    left: Link<Node23<K, V>>,
-    right: Link<Node23<K, V>>,
-    size: uint,
 }
 
-struct Node3<K, V> {
-    key1: K,
-    value1: V,
-    key2: K,
-    value2: V,
-    left: Link<Node23<K, V>>,
-    middle: Link<Node23<K, V>>,
-    right: Link<Node23<K, V>>,
-    size: uint,
+struct InteriorNode<'a, K, V> {
+    field1: &'a K, // smallest key for a leaf node descending from child 2
+    field2: Option<&'a K>, // smallest key for a leaf node descending from child 3
+    child1: Box<Node23<'a, K, V>>,
+    child2: Box<Node23<'a, K, V>>,
+    child3: Option<Box<Node23<'a, K, V>>>,
 }
 
-enum Node23<K, V> {
-    Node2(Node2<K, V>),
-    Node3(Node3<K, V>),
+enum Node23<'a, K, V> {
+    Leaf(LeafNode<K, V>),
+    Interior(InteriorNode<'a, K, V>),
 }
 
-impl<K, V> Node23<K, V> {
-    fn leaf2(key: K, value: V) -> Node23<K, V> {
-        Node2(Node2 { key: key, value: value, 
-                      left: None, right: None, size: 1 })
-    }
+pub struct Tree23<'a, K, V> {
+    root: Option<Node23<'a, K, V>>,
+}
 
-    fn leaf3(key1: K, value1: V, key2: K, value2: V) -> Node23<K, V> {
-        Node3(Node3 { key1: key1, value1: value1, key2: key2, value2: value2, 
-                      left: None, middle: None, right: None, size: 1 })
-    }
+impl<'a, K, V> Tree23<'a, K, V> {
+    pub fn new() -> Tree23<'a, K, V> { Tree23 { root: None } }
 
-    fn is2(&self) -> bool {
-        match *self {
-            Node2(_) => true,
-            _        => false,
-        }
-    }
-
-    fn is3(&self) -> bool {
-        match *self {
-            Node3(_) => true,
-            _        => false,
+    fn leaf(key: K, value: V) -> Tree23<'a, K, V> {
+        Tree23 { 
+            root: Some( Leaf(LeafNode { key: key, value: value }) ) 
         }
     }
 }
 
-fn getlink<K: Ord, V: Copy>(link: &Link<Node23<K,V>>, key: &K) -> Option<V> {
-    match *link {
-        None => None,
-        Some(ref b) => (*b).get(key)
+impl<K: Ord, V: Copy> LeafNode<K, V> {
+    pub fn get(&self, key: &K) -> Option<V> {
+        if key == &self.key {
+            Some(self.value)
+        } else {
+            None
+        }
     }
 }
 
-fn insertlink<K: Ord, V: Copy>(link: &mut Link<Node23<K,V>>, key: K, value: V) {
-    match *link {
-        None => *link = Some(box Node23::leaf2(key, value)),
-        Some(ref mut b) => (*b).insert(key, value)
+impl<'a, K: Ord, V: Copy> InteriorNode<'a, K, V> {
+    pub fn get(&self, key: &K) -> Option<V> {
+        match key.cmp(self.field1) {
+            Less => self.child1.get(key),
+            Equal => self.child2.get(key),
+            Greater =>
+                if self.child3.is_some() {
+                    match key.cmp(self.field2.unwrap()) {
+                        Less => self.child2.get(key),
+                        _ => self.child3.get_ref().get(key),
+                    }
+                } else {
+                    self.child2.get(key)
+                }
+        }
     }
 }
 
-impl<K: Ord, V: Copy> Node23<K, V> {
+impl<'a, K: Ord, V: Copy> Node23<'a, K, V> {
     fn get(&self, key: &K) -> Option<V> {
         match *self {
-            Node2(ref n) =>
-                match key.cmp(&n.key) {
-                    Equal => Some(n.value),
-                    Less => getlink(&n.left, key),
-                    Greater => getlink(&n.right, key),
-                },
-            Node3(ref n) =>
-                match key.cmp(&n.key1) {
-                    Equal => Some(n.value1),
-                    Less => getlink(&n.left, key),
-                    Greater =>
-                        match key.cmp(&n.key2) {
-                            Equal => Some(n.value2),
-                            Less => getlink(&n.middle, key),
-                            Greater => getlink(&n.right, key),
-                        },
-                },
+            Leaf(ref leaf) => leaf.get(key),
+            Interior(ref node) => node.get(key),
         }
     }
 
+    /*
     fn insert(&mut self, key: K, value: V) {
         match *self {
             Node2(ref mut n) =>
@@ -104,26 +84,13 @@ impl<K: Ord, V: Copy> Node23<K, V> {
             Node3(ref mut n) => {}
         }
     }
+    */
 }
 
 
-pub struct Tree23<K, V> {
-    root: Option<Node23<K, V>>,
-}
 
-impl<K, V> Tree23<K, V> {
-    pub fn new() -> Tree23<K, V> { Tree23 { root: None } }
 
-    pub fn leaf2(key: K, value: V) -> Tree23<K, V> {
-        Tree23 { root: Some(Node23::leaf2(key, value)) }
-    }
-
-    pub fn leaf3(key1: K, value1: V, key2: K, value2: V) -> Tree23<K, V> {
-        Tree23 { root: Some(Node23::leaf3(key1, value1, key2, value2)) }
-    }
-}
-
-impl<K: Ord, V: Copy> Tree23<K, V> {
+impl<'a, K: Ord, V: Copy> Tree23<'a, K, V> {
     pub fn get(&self, key: &K) -> Option<V> {
         match self.root {
             None => None,
@@ -131,6 +98,7 @@ impl<K: Ord, V: Copy> Tree23<K, V> {
         }
     }
 
+    /*
     pub fn insert(&mut self, key: K, value: V) {
         match self.root {
             None => {
@@ -141,6 +109,7 @@ impl<K: Ord, V: Copy> Tree23<K, V> {
             }
         }
     }
+    */
 }
 
 
